a0c239fa515bae362942ebe8fe114c2902939444
LukzST <lucaseduarte6@gmail.com>
2026-06-02 22:04:41 +0800

    aaa

---
+++ 02-06-26/estacao_metereologica/app/pages/login.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+export default function Login({ navigation }) {
+    return (
+        <View>
+            <Text>
+                Página de Login
+            </Text>
+        </View>
+    )
+}
